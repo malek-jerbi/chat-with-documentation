54b80d13e0307071a9773c5310c685b2e55c39d4
Malek Jerbi <malek.jerbi@gmail.com>
2023-06-04 16:42:03 -0400

    ingestion

--- ingestion.py
+++ ingestion.py
@@ -12,27 +12,24 @@ pinecone.init(
 
 
 def ingest_docs() -> None:
-    loader = ReadTheDocsLoader(path="langchain-docs/langchain.readthedocs.io/en/latest")
+    loader = ReadTheDocsLoader(path="langchain-docs", encoding="utf-8")
     raw_documents = loader.load()
-    print(f"loaded {len(raw_documents) }documents")
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""]
-    )
+    print(f"loaded {len(raw_documents)} documents")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""])
     documents = text_splitter.split_documents(documents=raw_documents)
-    print(f"Splitted into {len(documents)} chunks")
-
+    print(f"Splitted into {len(documents)} documents")
+    
     for doc in documents:
-        old_path = doc.metadata["source"]
-        new_url = old_path.replace("langchain-docs", "https:/")
+        old_path= doc.metadata["source"]
+        new_url = old_path.replace("langchain-docs\\", "https://").replace("\\", "/")
         doc.metadata.update({"source": new_url})
 
-    print(f"Going to insert {len(documents)} to Pinecone")
-    embeddings = OpenAIEmbeddings()
-    Pinecone.from_documents(
-        documents[3969:], embeddings, index_name="langchain-doc-index"
-    )
-    print("****** Added to Pinecone vectorstore vectors")
 
+    print(f"Going to insert {len(documents)} documents into Pinecone")
+    embeddings = OpenAIEmbeddings()
+
+    Pinecone.from_documents(documents, embeddings, index_name="langchain-doc-index")
+    print("********* Added to Pinecone vectorstore *********")
 
 if __name__ == "__main__":
     ingest_docs()
